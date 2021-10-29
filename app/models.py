1b792f9d69cbd541efd656b8fa8f6f879d6d1f1d
TheCaffeine <85110314+TheCaffeine@users.noreply.github.com>
2021-10-29 19:23:48 +0300

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,49 @@
+class Source:
+    '''
+    Source class to define the News source Objects
+    '''
+    def __init__(self, id, name, description, source_url):
+        self.id = id
+        self.name = name
+        self.description= description
+        self.url = source_url
+
+
+class Article:
+    '''
+    Articles class model to define the article Objects
+    '''
+    def __init__(self, title, urlToImage, desrciption, author, publishedAt, url, content):
+        self.title = title
+        self.urlToImage =urlToImage
+        self.description = desrciption
+        self.author = author
+        self.publishedAt = publishedAt
+        self.url = url
+        self.content =content
+
+class Category:
+    '''
+    Article class inistance for categories objects of the news sources
+    '''
+    def __init__(self, title, urlToImage, desrciption, author, publishedAt, url, content):
+        self.title = title
+        self.urlToImage =urlToImage
+        self.description = desrciption
+        self.author = author
+        self.publishedAt = publishedAt
+        self.url = url
+        self.content =content
+
+class Headlines:
+    '''
+    Headlines class inistance for headlines objects of the news sources
+    '''
+    def __init__(self, title, urlToImage, desrciption, author, publishedAt, url, content):
+        self.title = title
+        self.urlToImage =urlToImage
+        self.description = desrciption
+        self.author = author
+        self.publishedAt = publishedAt
+        self.url = url
+        self.content =content
---
+++ start.sh
@@ -0,0 +1,3 @@
+export API_KEY = ffc5892b0e01428f8dd49bca764e3fd9
+
+python3 manage.py server
